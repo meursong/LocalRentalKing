3bd52e6ee33063dc8f24f52f676901f6fe21dfcd
TheON2 <sniperad@naver.com>
2022-02-28 10:26:55 +0900

    DW:front layout modify

--- front/components/AppLayout/AppLayout.js
+++ front/components/AppLayout/AppLayout.js
@@ -1,24 +1,18 @@
 import Link from 'next/link';
 import PropTypes from 'prop-types';
-import React, { useCallback } from 'react';
 import { Menu, Input, Row, Col, Button } from 'antd';
-import {LeftSidebar, Navbar, RightSidebar, SearchInput, SidebarDiv, SidebarImg, SidebarLi, SidebarUl} from './styles';
 import 'antd/dist/antd.css';
 import { useDispatch, useSelector } from 'react-redux';
+import useInput from '../../hooks/useInput';
+import React, { useCallback } from 'react';
 import Router from 'next/router';
 
-import useInput from '../../hooks/useInput';
-import icon1 from '../../public/favicon.png';
-import icon2 from '../../public/home2.png';
-import icon3 from '../../public/search.png';
-import icon4 from '../../public/bookmark.png';
-import icon5 from '../../public/clipboard.png';
-import icon6 from '../../public/user.png';
-import icon7 from '../../public/bell.png';
-import icon8 from '../../public/email.png';
-import icon9 from '../../public/more.png';
-import icon10 from '../../public/logout.png';
+import {LeftSidebar, Navbar, RightSidebar, SearchInput, SidebarDiv, SidebarImg, SidebarLi, SidebarUl} from './styles';
+
 import { logoutRequestAction } from '../../reducers/user';
+
+import icon4 from '../../public/bookmark.png';
+
 
 function AppLayout({ children }) {
   const [searchInput, onChangeSearchInput] = useInput('');
@@ -59,48 +53,42 @@ function AppLayout({ children }) {
               <SidebarUl>
                 <Link href="/">
                   <SidebarLi>
-                    <SidebarImg src={icon1} />
+
                   </SidebarLi>
                 </Link>
                 <Link href="/">
                   <SidebarLi>
-                    <SidebarImg src={icon2} /><a> 홈</a>
+                    <a> 물건을 빌려줘</a>
                   </SidebarLi>
                 </Link>
                 <Link href="/related">
                   <SidebarLi>
-                    <SidebarImg src={icon3} /><a> 탐색하기</a>
+                    <a> 물건을 빌려줄게</a>
                   </SidebarLi>
                 </Link>
-                {/* <Link href={`/liked/${me.id}`}> */}
-                {/* <SidebarLi> */}
-                {/*   <SidebarImg src={icon4} /><a> 북마크</a> */}
-                {/* </SidebarLi> */}
-                {/* </Link> */}
-                {/* 이부분이 버그가 생기고 있어 회원가입이 터져서 일단 비활성화 했습니다. */}
                 <Link href="/list">
                   <SidebarLi>
-                    <SidebarImg src={icon5} /><a> 리스트</a>
+                    <a> 힘을 빌려줘</a>
                   </SidebarLi>
                 </Link>
                 <Link href="/profile">
                   <SidebarLi>
-                    <SidebarImg src={icon6} /><a> 프로필</a>
+                    <a> 힘을 빌려줄게</a>
                   </SidebarLi>
                 </Link>
                 <Link href="/">
                   <SidebarLi>
-                    <SidebarImg src={icon7} /><a> 알림</a>
+                    <a> 같이 하자</a>
                   </SidebarLi>
                 </Link>
                 <Link href="/">
                   <SidebarLi>
-                    <SidebarImg src={icon8} /><a> 쪽지</a>
+                    <a> 동네 놀이터</a>
                   </SidebarLi>
                 </Link>
                 <Link href="/">
                   <SidebarLi>
-                    <SidebarImg src={icon9} /><a> 더보기</a>
+                    <a> 동네 친구 찾기</a>
                   </SidebarLi>
                 </Link>
                 <SidebarLi>
@@ -108,7 +96,7 @@ function AppLayout({ children }) {
                 </SidebarLi>
                 <div onClick={onLogOut}>
                   <SidebarLi>
-                    <SidebarImg src={icon10} /><a> 로그아웃</a>
+                    <a> 로그아웃</a>
                   </SidebarLi>
                 </div>
               </SidebarUl>
