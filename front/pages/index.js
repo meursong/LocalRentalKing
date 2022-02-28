4b521b9470bccc92e6a4f92e3b29dc685775f9bd
TheON2 <sniperad@naver.com>
2022-02-28 18:08:25 +0900

    DW: mainpages,redux,reducers,sagas

--- front/pages/index.js
+++ front/pages/index.js
@@ -1,37 +1,32 @@
 import React, { useEffect } from 'react';
+import wrapper from '../store/configureStore';
+import axios from 'axios';
 import { useDispatch, useSelector } from 'react-redux';
 import { useInView } from 'react-intersection-observer';
-import axios from 'axios';
 import { END } from 'redux-saga';
 import { Col, Row } from 'antd';
+
 import AppLayout from '../components/AppLayout/AppLayout';
 import PostForm from '../components/PostForm';
 import PostCard from '../components/PostCard';
-import { LOAD_POST_REQUEST } from '../reducers/post';
-import { LOAD_MY_INFO_REQUEST } from '../reducers/user';
-import wrapper from '../store/configureStore';
 import LoginForm from '../components/LoginForm';
+
+import { LOAD_MY_INFO_REQUEST } from '../reducers/user';
+import { LOAD_POST_REQUEST } from '../reducers/post';
 
 function Home() {
   const dispatch = useDispatch();
   const { me } = useSelector((state) => state.user);
   const { mainPosts, hasMorePost, loadPostLoading, reTweetError, id } = useSelector((state) => state.post);
-
-  useEffect(() => {
-    if (reTweetError) {
-      alert(reTweetError);
-    }
-  }, [reTweetError]);
-
   const [ref, inView] = useInView();
 
-  useEffect(
+  useEffect( // 화면 사이즈에 따라 버그가 발생중 fix1
     () => {
       if (inView && hasMorePost && !loadPostLoading) {
-        const lastId = mainPosts[mainPosts.length - 1]?.id;
+        const lastId = mainPosts[mainPosts.length - 1]?.id; // 인피니트 스크롤 구현을 위해 프론트 서버의 현재 렌더링중인 게시글들중 가장 아래 게시물의 게시넘버를 lastId로
         dispatch({
           type: LOAD_POST_REQUEST,
-          lastId,
+          lastId, // 게시물 10개를 요청하고 인피니트 스크롤 구현을 위해 lastId를 전송하여 lastId 기준으로 10개를 잘라 받아온다.
         });
       }
     },
@@ -45,6 +40,7 @@ function Home() {
         {me && <PostForm />}
         {mainPosts.map((post) => <PostCard key={post.id} post={post} />)}
         <div ref={hasMorePost && !loadPostLoading ? ref : undefined} />
+      {/* 아직 게시물을 전부 열람하지 않았고 && 게시물을 요청하는 중이 아닐경우 인피니트 스크롤 동작 : 아닐경우 undefined */}
       </AppLayout>
     ) : (
       <Row gutter={8}>
@@ -52,17 +48,10 @@ function Home() {
           <img src="main.png" style={{ height: 950 }} />
         </Col>
         <Col xs={24} md={10}>
-          <h1>오늘은 무슨 일이 일어났나요?</h1>
+          <h1>우리동네 렌탈대장을 지금 이용해 보세요!</h1>
           <LoginForm style={{ marginTop: 300 }} />
         </Col>
         <Col xs={24} md={4}>
-          <a
-            href="https://github.com/TheON2"
-            target="_blank"
-            rel="noreferrer noopener"
-          >
-            Made by TheON2
-          </a>
         </Col>
       </Row>
     )}
--- front/sagas/user.js
+++ front/sagas/user.js
@@ -1,9 +1,181 @@
 import {all, call, fork, put, takeLatest, throttle} from 'redux-saga/effects';
 import axios from 'axios';
-import { } from '../reducers/post';
-import { } from '../reducers/user';
+import {
+  CHANGE_NICKNAME_FAILURE,
+  CHANGE_NICKNAME_REQUEST,
+  CHANGE_NICKNAME_SUCCESS,
+  LOAD_MY_INFO_FAILURE,
+  LOAD_MY_INFO_REQUEST,
+  LOAD_MY_INFO_SUCCESS,
+  LOAD_USER_FAILURE,
+  LOAD_USER_REQUEST,
+  LOAD_USER_SUCCESS,
+  LOAD_USERS_FAILURE,
+  LOAD_USERS_REQUEST,
+  LOAD_USERS_SUCCESS,
+  LOG_IN_FAILURE,
+  LOG_IN_REQUEST,
+  LOG_IN_SUCCESS,
+  LOG_OUT_FAILURE,
+  LOG_OUT_REQUEST,
+  LOG_OUT_SUCCESS,
+  SIGN_UP_FAILURE,
+  SIGN_UP_REQUEST,
+  SIGN_UP_SUCCESS,
+} from '../reducers/user';
 
-export default function* postSaga() {
+
+function loadMyInfoAPI() {
+  return axios.get('/user');
+}
+function SignUpAPI(data) {
+  return axios.post('/user', data);
+}
+function loadUserAPI(data) {
+  return axios.get(`/user/${data}`);
+}
+function loadUsersAPI() {
+  return axios.get('/user/all');
+}
+function ChangeNickNameAPI(data) {
+  return axios.patch('/user/nickname', {nickname: data});
+}
+function logInAPI(data) {
+  return axios.post('/user/login', data);
+}
+function logOutAPI(data) {
+  return axios.post('/user/logout', data);
+}
+
+
+function* loadMyInfo(action) {
+  try {
+    const result = yield call(loadMyInfoAPI, action.data);
+    yield put({ // put이 액션을 dispatch하는 역할과 빗슷하게 본다
+      type: LOAD_MY_INFO_SUCCESS,
+      data: result.data,
+    });
+  } catch (err) {
+    yield put({
+      type: LOAD_MY_INFO_FAILURE,
+      error: err.response.data,
+    });
+  }
+}
+function* loadUser(action) {
+  try {
+    const result = yield call(loadUserAPI, action.data);
+    yield put({ // put이 액션을 dispatch하는 역할과 빗슷하게 본다
+      type: LOAD_USER_SUCCESS,
+      data: result.data,
+    });
+  } catch (err) {
+    yield put({
+      type: LOAD_USER_FAILURE,
+      error: err.response.data,
+    });
+  }
+}
+function* loadUsers(action) {
+  try {
+    const result = yield call(loadUsersAPI, action.data);
+    yield put({ // put이 액션을 dispatch하는 역할과 빗슷하게 본다
+      type: LOAD_USERS_SUCCESS,
+      data: result.data,
+    });
+  } catch (err) {
+    yield put({
+      type: LOAD_USERS_FAILURE,
+      error: err.response.data,
+    });
+  }
+}
+function* logIn(action) {
+  try {
+    const result = yield call(logInAPI, action.data);
+    yield put({ // put이 액션을 dispatch하는 역할과 빗슷하게 본다
+      type: LOG_IN_SUCCESS,
+      data: result.data,
+    });
+  } catch (err) {
+    yield put({
+      type: LOG_IN_FAILURE,
+      error: err.response.data,
+    });
+  }
+}
+function* logOut(action) {
+  try {
+    yield call(logOutAPI, action.data);
+    yield put({ // put이 액션을 dispatch하는 역할과 빗슷하게 본다
+      type: LOG_OUT_SUCCESS,
+    });
+  } catch (err) {
+    yield put({
+      type: LOG_OUT_FAILURE,
+      error: err.response.data,
+    });
+  }
+}
+function* signUp(action) {
+  try {
+    yield call(SignUpAPI, action.data);
+    yield put({ // put이 액션을 dispatch하는 역할과 빗슷하게 본다
+      type: SIGN_UP_SUCCESS,
+    });
+  } catch (err) {
+    yield put({
+      type: SIGN_UP_FAILURE,
+      error: err.response.data,
+    });
+  }
+}
+function* changeNickName(action) {
+  try {
+    const result = yield call(ChangeNickNameAPI, action.data);
+    yield put({ // put이 액션을 dispatch하는 역할과 빗슷하게 본다
+      type: CHANGE_NICKNAME_SUCCESS,
+      data: result.data,
+    });
+  } catch (err) {
+    yield put({
+      type: CHANGE_NICKNAME_FAILURE,
+      error: err.response.data,
+    });
+  }
+}
+
+
+function* watchLogin() {
+  yield takeLatest(LOG_IN_REQUEST, logIn);
+}
+function* watchLogOut() {
+  yield takeLatest(LOG_OUT_REQUEST, logOut);
+}
+function* watchSignUp() {
+  yield takeLatest(SIGN_UP_REQUEST, signUp);
+}
+function* watchLoadMyInfo() {
+  yield takeLatest(LOAD_MY_INFO_REQUEST, loadMyInfo);
+}
+function* watchChangenickName() {
+  yield takeLatest(CHANGE_NICKNAME_REQUEST, changeNickName);
+}
+function* watchLoadUserInfo() {
+  yield takeLatest(LOAD_USER_REQUEST, loadUser);
+}
+function* watchLoadUsersInfo() {
+  yield takeLatest(LOAD_USERS_REQUEST, loadUsers);
+}
+
+export default function* userSaga() {
   yield all([
+    fork(watchLogin),
+    fork(watchLogOut),
+    fork(watchSignUp),
+    fork(watchLoadMyInfo),
+    fork(watchLoadUserInfo),
+    fork(watchLoadUsersInfo),
+    fork(watchChangenickName),
   ]);
 }
