6fa1ccef910340c49ab187346b3ae74c9104efb2
Meursong <feelssu@gmail.com>
2022-03-08 20:57:55 +0900

    su/Integ_0.0.0(front folder deleted)

--- front/sagas/post.js
+++
@@ -1,9 +0,0 @@
-import {all, call, fork, put, takeLatest, throttle} from 'redux-saga/effects';
-import axios from 'axios';
-import { } from '../reducers/post';
-import { } from '../reducers/user';
-
-export default function* postSaga() {
-  yield all([
-  ]);
-}
